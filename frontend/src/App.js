7ae32c86077d7b9c42a8e7951312ced8d1250ca2
Aneesh Komanduri <akomand@MacBook-Pro.local>
2021-02-03 22:58:32 -0600

    registration ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Home } from './components/Home/Home'
 import { HomeNavBar } from './components/NavBar/NavBar'
 import { Rate } from './components/Rate/Rate'
+import { Register } from './components/Register/Register'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HomeNavBar />
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/register' component={Register}/>
         <Route path='/rate' component={Rate}/>
       </Switch>
     </div>
