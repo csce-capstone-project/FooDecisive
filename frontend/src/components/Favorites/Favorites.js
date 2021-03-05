2ce01612d75d82f88c95a33e66e9281a21487379
Aneesh Komanduri <akomand@MacBook-Pro.local>
2021-03-04 22:04:35 -0600

    changed rating to stars ui

--- frontend/src/components/Favorites/Favorites.js
+++ frontend/src/components/Favorites/Favorites.js
@@ -1,24 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import Card from '@material-ui/core/Card';
-import { CardActionArea, DialogContent, Button } from '@material-ui/core';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Box from '@material-ui/core/Box';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import StarIcon from '@material-ui/icons/Star';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import {authFetch, useAuth} from "../../services/authentication";
-import Form from "react-bootstrap/Form";
+import {authFetch} from "../../services/authentication";
 import AddIcon from '@material-ui/icons/Add';
-
+import Pagination from '@material-ui/lab/Pagination';
 import { yelpBusID } from '../../services/yelp';
 import {FavoritesList} from './FavoritesList';
 
@@ -35,7 +19,6 @@ const useStyles = makeStyles((theme) => ({
 
 export function Favorites() {
   const [results, setResults] = useState([]);
-  const [promise, setPromise] = useState([]);
 
 
   useEffect(() => {
@@ -51,12 +34,6 @@ export function Favorites() {
         let bus = []   
         for(let i = 0; i < businesses.length; i++) {
           bus.push(yelpBusID.searchByID(businesses[i][0]))
-          // yelpBusID.searchByID(businesses[i][0]).then(business => {
-          //   console.log(business)
-          //   bus.push(business)
-          //   setResults(bus)
-          //   console.log(results)
-          // })
         }  
 
         Promise.all(bus).then((res) => {
@@ -74,7 +51,12 @@ export function Favorites() {
 
   }, [])
 
-  return(<FavoritesList businesses={results} />);
+  return(
+    <div>
+      <Pagination count={10} />
+      <FavoritesList businesses={results} />
+    </div>
+  );
     
 }
       
